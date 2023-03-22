d478f2a14c432f4763f61d25b94ec197a4184303
lpl6448 <lpl6448@rit.edu>
2023-03-22 19:17:38 -0400

    Coin pickup animation

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Coin : DynamicObject
 {
+    [SerializeField]
+    private Animation collectAnimation;
+
     // Start is called before the first frame update
     public override bool IsTraversable(DynamicObject mover)
     {
@@ -13,5 +16,21 @@ public class Coin : DynamicObject
     public override bool CanMove(Vector3Int tilePosition)
     {
         return false;
+    }
+
+    public override void DestroyObject(object context)
+    {
+        // StartCoroutine is used here instead of StartAnimation so that the coin pickup animation
+        // doesn't delay the next player input.
+        StartCoroutine(WaitToDestroy());
+    }
+
+    private IEnumerator WaitToDestroy()
+    {
+        yield return new WaitForSeconds(AnimationUtility.StandardAnimationDuration / 4);
+        collectAnimation.Play();
+        while (collectAnimation.isPlaying)
+            yield return null;
+        Destroy(gameObject);
     }
 }
