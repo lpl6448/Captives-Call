f15e51982fbe397d9104991eddcad11a760b407e
lpl6448 <lpl6448@rit.edu>
2023-03-25 12:30:23 -0400

    Experimental powerup collect animation

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PowerUp : DynamicObject
 {
+    [SerializeField]
+    private ParticleSystem Particles;
+    [SerializeField]
+    private Animation Animation;
+
     public override bool IsTraversable(DynamicObject mover)
     {
         return true;
@@ -21,8 +26,18 @@ public class PowerUp : DynamicObject
 
     private IEnumerator WaitToDestroy()
     {
-        yield return new WaitForSeconds(AnimationUtility.StandardAnimationDuration / 2);
+        yield return new WaitForSeconds(AnimationUtility.StandardAnimationDuration / 4);
         StopAnimation();
+
+        Animation.Play();
+        Particles.Play();
+
+        yield return new WaitForSeconds(0.75f);
+        Particles.Stop();
+        Particles.Emit(60);
+
+        yield return new WaitForSeconds(2);
+
         Destroy(gameObject);
     }
 }
